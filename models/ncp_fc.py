1a431cce103fd1d6c1ee0c5a6f58225ac2d8797a
minhhieutruong0705 <minhhieutruong0705@gmail.com>
2022-02-13 21:31:27 +0700

    NCP Layer

---
+++ models/ncp_fc.py
@@ -0,0 +1,77 @@
+from pyexpat import model
+import torch
+import torch.nn as nn
+import kerasncp as kncp
+from kerasncp.torch import LTCCell
+
+
+# nn.Module that unfolds a RNN cell into a sequence
+class RNNSequence(nn.Module):
+    def __init__(self, rnn_cell):
+        super().__init__()
+        self.rnn_cell = rnn_cell
+
+    def forward(self, x):
+        device = x.device
+        batch_size = x.size(0)
+        seq_len = x.size(1)
+        # init hidden state
+        hidden_state = torch.zeros(
+            (batch_size, self.rnn_cell.state_size), device=device)
+        outputs = []
+        for t in range(seq_len):
+            inputs = x[:, t]
+            new_output, hidden_state = self.rnn_cell.forward(
+                inputs, hidden_state)
+            outputs.append(new_output)
+        outputs = torch.stack(outputs, dim=1)  # return entire sequence
+        return outputs
+
+
+"""
+NCP_FC utilizes the dynamicity of NCP to optimize the weight 
+of conventional fully-connected layers of CNN classifiers.
+Default values of NCP architecture are borrowed from the original work of NCP
+(Neural circuit policies enabling auditable autonomy by Mathias Lechner et. al., Oct 2020)
+"""
+class NCP_FC(nn.Module):
+    def __init__(self,
+                 seq_len,
+                 classes=2,
+                 sensory_neurons=32,
+                 inter_neurons=12,
+                 command_neurons=6,
+                 motor_neurons=1,
+                 sensory_outs=6,
+                 inter_outs=4,
+                 recurrent_dense=6,
+                 motor_ins=6
+                 ):
+        super().__init__()
+
+        wiring = kncp.wirings.NCP(
+            inter_neurons=inter_neurons,
+            command_neurons=command_neurons,
+            motor_neurons=motor_neurons,
+            sensory_fanout=sensory_outs,
+            inter_fanout=inter_outs,
+            recurrent_command_synapses=recurrent_dense,
+            motor_fanin=motor_ins,
+        )
+        ltc_cell = LTCCell(wiring=wiring, in_features=sensory_neurons)
+        self.ltc_seq = RNNSequence(ltc_cell)
+
+        self.fc = nn.Linear(motor_neurons * seq_len, classes)
+
+    def forward(self, x):
+        x = self.ltc_seq(x)
+        x = torch.flatten(x, start_dim=1)
+        return self.fc(x)
+
+
+if __name__ == '__main__':
+    x = torch.randn(8, 32, 64)  # (batch_size, sequence_length, features)
+    model = NCP_FC(seq_len=32, sensory_neurons=64, classes=2)
+    y = model(x)
+    assert y.size() == (8, 2)
+    print("[ASSERTION] NCP_FC OK!")
