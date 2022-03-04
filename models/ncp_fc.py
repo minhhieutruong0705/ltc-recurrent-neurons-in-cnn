56e3751bbe314e59c770871033646dea8751a2b9
minhhieutruong0705 <minhhieutruong0705@gmail.com>
2022-03-04 22:49:32 +0700

    bidirectional ncp

--- models/ncp_fc.py
+++ models/ncp_fc.py
@@ -34,10 +34,13 @@ of conventional fully-connected layers of CNN classifiers.
 Default values of NCP architecture are borrowed from the original work of NCP
 (Neural circuit policies enabling auditable autonomy by Mathias Lechner et. al., Oct 2020)
 """
+
+
 class NCP_FC(nn.Module):
     def __init__(self,
                  seq_len,
                  classes=2,
+                 bỉ_directional=False,
                  sensory_neurons=32,
                  inter_neurons=12,
                  command_neurons=6,
@@ -49,6 +52,10 @@ class NCP_FC(nn.Module):
                  ):
         super().__init__()
 
+        # init
+        self.bi_directional = bỉ_directional
+
+        # ncp wiring
         wiring = kncp.wirings.NCP(
             inter_neurons=inter_neurons,
             command_neurons=command_neurons,
@@ -58,20 +65,33 @@ class NCP_FC(nn.Module):
             recurrent_command_synapses=recurrent_dense,
             motor_fanin=motor_ins,
         )
-        ltc_cell = LTCCell(wiring=wiring, in_features=sensory_neurons)
-        self.ltc_seq = RNNSequence(ltc_cell)
 
-        self.fc = nn.Linear(motor_neurons * seq_len, classes)
+        # forward
+        ltc_cell_fwd = LTCCell(wiring=wiring, in_features=sensory_neurons)
+        self.ltc_fwd_seq = RNNSequence(ltc_cell_fwd)
+        if self.bi_directional:
+            ltc_cell_bwd = LTCCell(wiring=wiring, in_features=sensory_neurons)
+            self.ltc_bwd_seq = RNNSequence(ltc_cell_bwd)
+
+        # reduce to number of classes
+        directions = 2 if self.bi_directional else 1
+        self.fc = nn.Linear(motor_neurons * seq_len * directions, classes)
 
     def forward(self, x):
-        x = self.ltc_seq(x)
+        # x: (B, S, C)
+        x_fw = self.ltc_fwd_seq(x)
+        if self.bi_directional:
+            x = x.flip(dims=[1])  # backward sequence
+            x_bw = self.ltc_bwd_seq(x)
+            x_bw = x_bw.flip(dims=[1])  # backward prediction for concatenation
+        x = x_fw if not self.bi_directional else torch.cat((x_fw, x_bw), dim=-1)
         x = torch.flatten(x, start_dim=1)
         return self.fc(x)
 
 
 if __name__ == '__main__':
     x = torch.randn(8, 32, 64)  # (batch_size, sequence_length, features)
-    model = NCP_FC(seq_len=32, sensory_neurons=64, classes=2)
+    model = NCP_FC(seq_len=32, sensory_neurons=64, classes=2, bỉ_directional=True)
     y = model(x)
     assert y.size() == (8, 2)
     print("[ASSERTION] NCP_FC OK!")
